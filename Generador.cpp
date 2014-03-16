82cea3bee96d462d56c6cfb8f9ddd2f490a5ef9e
Claudia Hernández <cghr1990@hotmail.com>
2014-03-16 17:55:01 -0600

    Esqueleto clase Generador

---
+++ Generador.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+
+class Generador {
+
+string genera(char op, string opdo1, string opdo2){}
+
+bool pushPOper(string op){}
+
+bool pushPilaO(string opdo){}
+
+void agregaFF {}
+
+void sacarFF {}
+
+bool PilaOempty {}
+
+bool POperempty {}
+
+void imprimePilaO {}
+
+void imprimePOper {}
+
+}
+
+
