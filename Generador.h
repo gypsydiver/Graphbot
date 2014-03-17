3d94587d8e433f71f4460ad482873d0c232d4cde
Fernando Mendoza <fermr91@gmail.com>
2014-03-16 19:16:51 -0600

    agrega genera a generador.h
    Conflicts:
    	Generador.cpp

--- Generador.h
+++ Generador.h
@@ -9,90 +9,75 @@ stack<string> POper;
 
 class Generador {
 
-
-public:
-
-void agregaFF();
-void sacarFF();
-
-string genera(string op, string opdo1, string opdo2){
-
+    stack<string> PilaO;
+    stack<string> POper;
+    int temporalActual;
     
+    public:
+        void agregaFF();
+        void sacarFF();
+
+        int tempActual(){
+            return temporalActual;
+        }
+
+        void genera(string op, string opdo1, string opdo2){
+            //imprime cuadrupo
+            cout << op << " " << opdo1 << " " << opdo2 << " " << temporalActual << endl;
+            
+            //agrega tempActual a PilaO
+            pushPilaO(to_string(temporalActual));
+
+            //actualiza temporal
+            temporalActual++;
+            cout<<"Temporal actual: "<<temporalActual<<endl;
+
+        }
+
+        string popPilaO() {
+            string top;
+            top = PilaO.top();
+            PilaO.pop();
+            return top;
+        }
+
+        string popPOper() {
+            string top;
+            top = POper.top();
+            POper.pop();
+            return top;
+        }
 
 
-}
+        void pushPOper(string op) {  
+            POper.push(op);
+        }
 
-string popPilaO() {
+        void pushPilaO(string op) {
+            PilaO.push(op);
+        }
 
-    string top;
+        bool PilaOEmpty() {
+            return PilaO.empty();
+        }
 
-    top = PilaO.top();
-    PilaO.pop();
-    return top;
+        bool POperEmpty(){
+            return POper.empty();
+        }
 
-}
+        void imprimePilaO(){
+            while(! PilaO.empty()){
+                cout << PilaO.top() << ", ";
+                PilaO.pop();
+            }
+        }
 
-string popPOper() {
-
-    string top;
-
-    top = POper.top();
-    POper.pop();
-    return top;
-
-}
-
-
-void pushPOper(string op) {
-        
-    POper.push(op);
-
-}
-
-void pushPilaO(string op) {
-        
-    PilaO.push(op);
-
-}
-
-bool PilaOempty() {
-    
-    if(PilaO.empty())
-    return true;
-    else
-    return false;
-}
-
-bool POperempty(){
-
-    if(POper.empty())
-    return true;
-    else
-    return false;
-}
-
-void imprimePilaO(){
-
-while(! PilaO.empty()){
-    
-    cout << PilaO.top() << ", ";
-
-    PilaO.pop();
-}
-    
-}
-
-void imprimePOper(){
-
-while(! POper.empty()){
-    
-    cout << POper.top() << ", ";
-
-    POper.pop();
-}
-
-}
-
+        void imprimePOper(){
+            while(! POper.empty()){
+                cout << POper.top() << ", ";
+                POper.pop();
+            }
+        }
 };
 
 
