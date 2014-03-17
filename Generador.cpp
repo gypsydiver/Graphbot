cf307d1fcd869a72e1f5008198101154689644a0
Claudia Hernández <cghr1990@hotmail.com>
2014-03-16 18:07:00 -0600

    Generador Actualizado

--- Generador.cpp
+++ Generador.cpp
@@ -3,24 +3,39 @@
 
 class Generador {
 
-string genera(char op, string opdo1, string opdo2){}
+stack<string> PilaO;
+stack<string> POper;
 
-bool pushPOper(string op){}
+public:
 
-bool pushPilaO(string opdo){}
-
-void agregaFF {}
-
-void sacarFF {}
-
-bool PilaOempty {}
-
-bool POperempty {}
-
-void imprimePilaO {}
-
-void imprimePOper {}
+string genera(string op, string opdo1, string opdo2);
+void pushPOper(string op);
+void pushPilaO(string opdo);
+void agregaFF();
+void sacarFF();
+bool PilaOempty();
+bool POperempty();
+void imprimePilaO();
+void imprimePOper();
+int tempActual();
 
 }
 
+string genera(string op, string opdo1, string opdo2){
 
+
+
+
+}
+
+void pushPOper(string op) {
+        
+    POper.push(op);
+
+}
+
+void pushPilaO(string op) {
+        
+    PilaO.push(op);
+
+}
